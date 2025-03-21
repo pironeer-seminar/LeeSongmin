1f58a57fb369ab5fd7303c060eb7c720e37e6251
songmin0111 <violet2111@naver.com>
2025-03-21 16:41:11 +0900

    ì´initial setting

---
+++ week2/week2_assignment/src/main/java/com/example/week2_assignment/Week2AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.week2_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2AssignmentApplication.class, args);
+	}
+
+}
---
+++ week2/week2_assignment/src/test/java/com/example/week2_assignment/Week2AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week2_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
