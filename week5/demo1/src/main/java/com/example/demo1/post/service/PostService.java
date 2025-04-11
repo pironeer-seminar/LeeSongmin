e214a82bf781733a3a764fd81b3dda547f15b811
songmin0111 <violet2111@naver.com>
2025-04-11 18:50:07 +0900

    [feat] Post 관련 throw문 커스텀 에러 핸들러로 변경 완료

--- week5/demo1/src/main/java/com/example/demo1/post/service/PostService.java
+++ week5/demo1/src/main/java/com/example/demo1/post/service/PostService.java
@@ -1,6 +1,7 @@
 package com.example.demo1.post.service;
 
 import com.example.demo1.common.exception.NotFoundException;
+import com.example.demo1.common.type.PostErrorType;
 import com.example.demo1.common.type.UserErrorType;
 import com.example.demo1.post.dto.request.PostCreateReq;
 import com.example.demo1.post.dto.request.PostUpdateReq;
@@ -48,7 +49,7 @@ public class PostService {
     public PostSearchRes detail(Long postId) {
         Post post = postRepository.findById(postId)
                 //.orElseThrow(() -> new IllegalArgumentException("조회된 게시글이 없습니다."));
-                .orElseThrow(() -> new NotFoundException(PostErrorType))
+                .orElseThrow(() -> new NotFoundException(PostErrorType.NOT_FOUND));
 
         return new PostSearchRes(post.getUser().getId(), post.getId(), post.getTitle(), post.getContent(),
                 post.getCreatedAt());
@@ -56,7 +57,8 @@ public class PostService {
 
     public Long update(Long postId, PostUpdateReq req) {
         Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("조회된 게시글이 없습니다."));
+                //.orElseThrow(() -> new IllegalArgumentException("조회된 게시글이 없습니다."));
+                        .orElseThrow(() -> new NotFoundException(PostErrorType.NOT_FOUND));
 
         post.update(req.getTitle(), req.getContent(), req.getStatus());
         postRepository.save(post);
