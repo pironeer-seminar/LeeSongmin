ddce23a62c54922b9aefb443521d786c7779d1f4
songmin0111 <violet2111@naver.com>
2025-04-11 19:41:36 +0900

    [feat] Validation Check 구현 추가

--- week5/demo1/src/main/java/com/example/demo1/post/dto/request/PostUpdateReq.java
+++ week5/demo1/src/main/java/com/example/demo1/post/dto/request/PostUpdateReq.java
@@ -2,17 +2,22 @@ package com.example.demo1.post.dto.request;
 
 import com.example.demo1.post.entity.PostStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class PostUpdateReq {
 
+    @NotBlank
     @Schema(description = "수정할 게시글 제목")
     private String title;
 
+    @NotBlank
     @Schema(description = "수정할 게시글 내용")
     private String content;
 
+    @NotNull
     @Schema(description = "공개/비공개")
     private PostStatus status;
 }
--- week5/demo1/src/main/java/com/example/demo1/user/dto/request/UserCreateReq.java
+++ week5/demo1/src/main/java/com/example/demo1/user/dto/request/UserCreateReq.java
@@ -1,11 +1,13 @@
 package com.example.demo1.user.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class UserCreateReq {
 
+    @NotNull
     @Schema(description = "유저 이름")
     private String name;
 }
